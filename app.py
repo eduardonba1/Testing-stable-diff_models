e9ca0adf1d6530f5e763a61311fbc34578eaebd1
eduardonba1 <142735616+eduardonba1@users.noreply.github.com>
2024-01-10 14:34:58 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ import os
 
 os.system(f"apt -y update -qq")
 os.system(f"apt -y install -qq aria2")
-os.system(f"pip install -q torch==2.0.0+cu118 torchvision==0.15.1+cu118 torchaudio==2.0.1+cu118 torchtext==0.15.1 torchdata==0.6.0 --extra-index-url https://download.pytorch.org/whl/cu118 -U")
-os.system(f"pip install -q xformers==0.0.19 triton==2.0.0 -U")
-os.system(f"pip install -q mediapipe==0.9.1.0 addict yapf fvcore omegaconf")
+os.system(f"pip install -q torch torchvision torchaudio torchtext torchdata --extra-index-url https://download.pytorch.org/whl/cu121 -U")
+os.system(f"pip install -q xformers triton -U")
+os.system(f"pip install -q mediapipe addict yapf fvcore omegaconf")
 
 os.system(f"git clone https://github.com/comfyanonymous/ComfyUI")
 os.chdir(f"/home/xlab-app-center/ComfyUI")
